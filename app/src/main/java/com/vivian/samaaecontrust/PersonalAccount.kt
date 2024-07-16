95bf7dc20b1923602a1206f16e3830c37219c3b0
Mary-Vivian <muthonimaryvivian>
2024-07-16 07:41:33 +0300

    Samaa Econtrust App

---
+++ app/src/main/java/com/vivian/samaaecontrust/PersonalAccount.kt
@@ -0,0 +1,20 @@
+package com.vivian.samaaecontrust
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class PersonalAccount : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_personal_account)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
